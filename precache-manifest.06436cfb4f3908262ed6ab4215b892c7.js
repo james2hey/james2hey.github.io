87d13f28bdf324b24b896e911d62433f8eebfbbd
James Toohey <jto59@uclive.ac.nz>
2019-03-03 20:28:03 +1300

    test

---
+++ precache-manifest.06436cfb4f3908262ed6ab4215b892c7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ef29465f27b7486195e",
+    "url": "/static/css/main.b4c2fc01.chunk.css"
+  },
+  {
+    "revision": "9ef29465f27b7486195e",
+    "url": "/static/js/main.9ef29465.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7881e5d9c3bccaf2d1f8",
+    "url": "/static/js/2.7881e5d9.chunk.js"
+  },
+  {
+    "revision": "543adf87ee381740f00d39febbfbace1",
+    "url": "/static/media/books.543adf87.svg"
+  },
+  {
+    "revision": "6dfa3b28b57255ce87e2ade7ecaffcc1",
+    "url": "/static/media/pencil.6dfa3b28.svg"
+  },
+  {
+    "revision": "55677c14d8e5615967862223c14d9fe2",
+    "url": "/static/media/computer.55677c14.svg"
+  },
+  {
+    "revision": "4bc6d12e5eac21d21d30a9e342f3cd31",
+    "url": "/static/media/mail.4bc6d12e.svg"
+  },
+  {
+    "revision": "e0ef542184ee61919d9581de959a678b",
+    "url": "/static/media/linkedin.e0ef5421.svg"
+  },
+  {
+    "revision": "e871c6174eec878a964f22bbfe0531ab",
+    "url": "/static/media/github.e871c617.svg"
+  },
+  {
+    "revision": "36d9e3d84e036caa6957749ccca4d74c",
+    "url": "/static/media/jamestoohey.36d9e3d8.svg"
+  },
+  {
+    "revision": "e2ac3f017605ffc06593432a1dbbd172",
+    "url": "/static/media/odms.e2ac3f01.svg"
+  },
+  {
+    "revision": "1020842d145c65d66eb5c7e496f7d98f",
+    "url": "/static/media/auctions.1020842d.svg"
+  },
+  {
+    "revision": "7d7a2f94e78e45d12cb64c00d8a13e97",
+    "url": "/static/media/snake.7d7a2f94.svg"
+  },
+  {
+    "revision": "47c85eb520430b283359c270fcf56bca",
+    "url": "/static/media/pedals.47c85eb5.svg"
+  },
+  {
+    "revision": "d92785db8dd6141e2250e2c7cb060fd2",
+    "url": "/static/media/vista.d92785db.svg"
+  },
+  {
+    "revision": "29bcefd9023c7a520ee6f1099dbfb04f",
+    "url": "/static/media/clubhub.29bcefd9.svg"
+  },
+  {
+    "revision": "1551585364848f984826943bb1302f7b",
+    "url": "/static/media/angular.15515853.svg"
+  },
+  {
+    "revision": "4c714a24d136a11e147fe6754d8d0680",
+    "url": "/static/media/c.4c714a24.svg"
+  },
+  {
+    "revision": "5e99ef1cd20d80a022c29df7e03835ae",
+    "url": "/static/media/java.5e99ef1c.svg"
+  },
+  {
+    "revision": "ba9bcb11ec14f859888315ef5419e0d0",
+    "url": "/static/media/kotlin.ba9bcb11.svg"
+  },
+  {
+    "revision": "c3ae250397f2507658492088193a1f1f",
+    "url": "/static/media/node.c3ae2503.svg"
+  },
+  {
+    "revision": "39d5b9c98fcd2b73dfd2f9283ea9f19a",
+    "url": "/static/media/python.39d5b9c9.svg"
+  },
+  {
+    "revision": "bd8afa24c4cc9dbe109767316936464d",
+    "url": "/static/media/react.bd8afa24.svg"
+  },
+  {
+    "revision": "e2fcf3cc6f4f9d0545c73046d7701d99",
+    "url": "/static/media/react-native.e2fcf3cc.svg"
+  },
+  {
+    "revision": "5e7ac8b3e1fb88fb357f6f1b93e193cf",
+    "url": "/static/media/sql.5e7ac8b3.svg"
+  },
+  {
+    "revision": "53aa17731627da8864e24bb8e0b87ba5",
+    "url": "/static/media/swift.53aa1773.svg"
+  },
+  {
+    "revision": "4331849c173883b330c2adc44c705fa1",
+    "url": "/static/media/vue.4331849c.svg"
+  },
+  {
+    "revision": "07c33849dafdb67d388b73ee6e2388a7",
+    "url": "/static/media/agile.07c33849.svg"
+  },
+  {
+    "revision": "b1d9228eccde8c5345064f91672022d9",
+    "url": "/static/media/waterfall.b1d9228e.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a863df7efb0a27d6c333b10bef772a41",
+    "url": "/index.html"
+  }
+];
